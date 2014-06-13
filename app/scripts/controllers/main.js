2e83b56233623f7c79cf3d230711f3f327800576
Tyler Canton <support@stepbusinessup.com>
2014-06-12 23:45:57 -0400

    Pushing and overriding files

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('chapter1App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
